e319734fd36bbf2a9530adabd40e2d1e788fac7d
Robert <robert@intellectionsoftware.com>
2016-06-30 17:28:10 +0200

    Preventing aliasing of bar graph

--- graphs/graphs.py
+++ graphs/graphs.py
@@ -19,7 +19,7 @@ def tag_dist(input_file):
         max_count = max(max_count, count)
         count_dist[count] = count_dist[count] + 1
     count_counts = [count_dist[i] for i in range(1, max_count + 1)]
-    bars = plt.bar([0.125 + x for x in range(max_count)], count_counts, log=True)
+    bars = plt.bar(range(max_count), count_counts, width=1.0, linewidth=0, log=True)
     plt.xlabel('Number of copies in bin')
     plt.ylabel('Unique ID\'s with bin size')
     plt.xticks([0.5 + x for x in range(max_count)], range(1, max_count+1))
