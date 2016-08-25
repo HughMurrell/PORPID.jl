01737dd8670130f8c56fbf0d1c885231113283b4
Robert <robert@intellectionsoftware.com>
2016-08-25 15:56:24 +0200

    Fixes based on testing

--- ProfileHMMModel.jl
+++ ProfileHMMModel.jl
@@ -3,8 +3,9 @@ module ProfileHMMModel
 
 using Nucleotides
 using Observations
+using States
 
-export string_to_state_model, viterbi
+export extract_tag
 
 const INSERTION_START_PROB = 0.01
 const INSERTION_EXTEND_PROB = 0.1
@@ -15,39 +16,13 @@ const L_PROBABILITY_OF_INSERTION = log(PROBABILITY_OF_INSERTION)
 const L_PROBABILITY_OF_DELETION = log(PROBABILITY_OF_DELETION)
 const L_PROBABILITY_OF_NORMAL_TRANSITION = log(1 - PROBABILITY_OF_DELETION - PROBABILITY_OF_INSERTION)
 const L_PROBABILITY_PER_EXTRA_BASE = log(0.05)
-const l_normal_transition = log(1 - (insertion_start_prob + deletion_start_prob))
-const l_start_insertion = log(insertion_start_prob)
-const l_extend_insertion = log(insertion_extend_prob)
-const l_end_insertion = log(1 - insertion_extend_prob)
-const l_start_deletion = log(deletion_start_prob)
-const l_extend_deletion = log(deletion_extend_prob)
-const l_end_deletion = log(1 - deletion_extend_prob)
-
-type ObservableState
-  index::Int64
-  value::DNASymbol
-end
-
-type RepeatingAnyState
-  index::Int64
-end
-
-type ProfileStateModel
-  n::Int64
-  expected::Array{States, 1}
-end
-
-function string_to_state_model(string_sequence::AbstractString)
-  # "NNNNNNNNNNCAGTTTAACTTTTGGGCCATCCATTCC*"
-  #         n   n    n    n    n    n             n    n
-  #      - I1  I2   I3   I4   I5   I6            I9   I10
-  #    /    \ /  \ /  \ /  \ /  \ /  \    n     /  \ /  \
-  # Start - S1 - S2 - S3 - S4 - S5 - S6 - S* - S8 - S9 - End
-  #     \      X    X    X    X    X    X    X    X    /
-  #       - D1 - D2 - D3 - D4 - D5 - D6 - D* - D8 - D9
-
-  return ProfileStateModel(length(string_sequence), string_sequence)
-end
+const L_NORMAL_TRANSITION = log(1 - (INSERTION_START_PROB + DELETION_START_PROB))
+const L_START_INSERTION = log(INSERTION_START_PROB)
+const L_EXTEND_INSERTION = log(INSERTION_EXTEND_PROB)
+const L_END_INSERTION = log(1 - INSERTION_EXTEND_PROB)
+const L_START_DELETION = log(DELETION_START_PROB)
+const L_EXTEND_DELETION = log(DELETION_EXTEND_PROB)
+const L_END_DELETION = log(1 - DELETION_EXTEND_PROB)
 
 function getDefault(array::Array{Float64,1}, index::Int64, default::Float64)
   if index < 1
@@ -72,15 +47,24 @@ function +(tt::TagTrace, float::Float64)
   TagTrace(tt.score + float, tt.tag)
 end
 
-function push!(tt::TagTrace, symbol::DNASymbol)
+function Base.push!(tt::TagTrace, symbol::DNASymbol)
   array = copy(tt.tag)
-  push!(array, symbol)
+  Base.push!(array, symbol)
   tt.tag = array
 end
 
 Base.isless(tt1::TagTrace, tt2::TagTrace) = Base.isless(tt1.score, tt2.score)
 
-function extract_tag(observations::Array{Observation,1}, model::ProfileStateModel)
+  # "nnnnnnnnnnCAGTTTAACTTTTGGGCCATCCATTCC*"
+  #         n   n    n    n    n    n             n    n
+  #      - I1  I2   I3   I4   I5   I6            I9   I10
+  #    /    \ /  \ /  \ /  \ /  \ /  \    n     /  \ /  \
+  # Start - S1 - S2 - S3 - S4 - S5 - S6 - S* - S8 - S9 - End
+  #     \      X    X    X    X    X    X    X    X    /
+  #       - D1 - D2 - D3 - D4 - D5 - D6 - D* - D8 - D9
+
+
+function extract_tag(observations::Array{Observation,1}, expected::Array{AbstractState, 1})
   # Probability of most likely path to end at state i, on observation j
   # S[i, j] = max(S[i-1, j-1] * normal_transition^, I[i, j-1] * end_insertion, D[i-1, j] * end_deletion^)
   # I[i, j] = max(S[i-1, j-1] * start_insertion, I[i, j-1] * continue_inserting)
@@ -88,56 +72,71 @@ function extract_tag(observations::Array{Observation,1}, model::ProfileStateMode
   # S*[i, j] = max(S[i, j-1], S[i-1, j-1], D[i-1, j])
   # D*[i, j] = max(s[i-1, j-1], D[i-1, j])
 
+  expected = sub(expected, 2:length(expected))
+  n = length(expected)
+
   D = fill(TagTrace(-Inf), n) # Start with log(0) = -Inf probability
   S = fill(TagTrace(-Inf), n)
   I = fill(TagTrace(-Inf), n+1)
 
   # Values for first observation
-  D[1] = TagTrace(l_start_deletion)
+  D[1] = TagTrace(L_START_DELETION)
   for i in 2:length(D)
-    D[i] = D[i-1] + l_extend_deletion
+    D[i] = D[i-1] + L_EXTEND_DELETION
   end
-  S[1] = TagTrace(l_normal_transition + prob(model.states[1].value, observations[1].value, observations[1].prob))
+  S[1] = TagTrace(L_NORMAL_TRANSITION + prob(expected[1].value, observations[1].value, observations[1].prob))
   for i in 2:length(S)
-    S[i] = D[i-1] + l_end_deletion + prob(model.states[i].value, observations[1].value, observations[1].prob)
+    if typeof(expected[i]) <: AbstractRepeatingAnyState
+      S[i] = D[i-1]
+    else
+      S[i] = D[i-1] + L_END_DELETION + prob(expected[i].value, observations[1].value, observations[1].prob)
+    end
   end
-  I[1] = TagTrace(l_start_insertion + prob(DNA_N, observations[1].value, observations[1].prob))
+  I[1] = TagTrace(L_START_INSERTION + prob(Nucleotides.DNA_N, observations[1].value, observations[1].prob))
+  for i in 1:length(S)
+    if typeof(expected[i]) <: AbstractBarcodeState
+      push!(S[i], observations[1].value)
+    end
+  end
 
   # Thereafter...
 
-  D[1] = -Inf # After first observation
+  D[1] = TagTrace(-Inf) # After first observation
   for observation in observations[2:end]
     for i in 2:length(D)
-      if model.states[i] <: RepeatingAnyState
+      if typeof(expected[i]) <: AbstractRepeatingAnyState
         D[i] = max(S[i-1], D[i-1]) # A repeating any state can be skipped with no penalty
       else
-        D[i] = max(S[i-1] + l_start_deletion, D[i-1] + l_extend_deletion)
+        D[i] = max(S[i-1] + L_START_DELETION, D[i-1] + L_EXTEND_DELETION)
       end
     end
 
-    I[n+1] = max(I[n+1] + l_extend_insertion, S[n] + l_start_insertion)
+    I[n+1] = max(I[n+1] + L_EXTEND_INSERTION, S[n] + L_START_INSERTION)
     for i in length(S):-1:2 # Backwards because we want to use the previous versions of s and i
-      if model.states[i] <: RepeatingAnyState
+      if typeof(expected[i]) <: AbstractRepeatingAnyState
         S[i] = max(S[i], S[i-1], D[i-1]) +
-          prob(DNA_N, observation.value, observation.prob)
+          prob(Nucleotides.DNA_N, observation.value, observation.prob)
         # No Insertion
-      elseif model.states[i-1] <: RepeatingAnyState
+      elseif typeof(expected[i-1]) <: AbstractRepeatingAnyState
         S[i] = max(S[i-1], D[i-1]) +
-               prob(model.states[i].value, observation.value, observation.prob)
+               prob(expected[i].value, observation.value, observation.prob)
         # No insertion
       else
-        S[i] = max(S[i-1] + l_normal_transition, I[i] + l_end_insertion, D[i-1] + l_end_deletion) +
-          prob(model.states[i].value, observation.value, observation.prob)
-        I[i] = max(I[i] + l_extend_insertion, S[i-1] + l_start_insertion) +
-          prob(DNA_N, observation.value, observation.prob)
-      end
-      if model.states[i] <: BarcodeState
-        push!(S[i], observation.value
+        S[i] = max(S[i-1] + L_NORMAL_TRANSITION, I[i] + L_END_INSERTION, D[i-1] + L_END_DELETION) +
+          prob(expected[i].value, observation.value, observation.prob)
+        I[i] = max(I[i] + L_EXTEND_INSERTION, S[i-1] + L_START_INSERTION) +
+          prob(Nucleotides.DNA_N, observation.value, observation.prob)
       end
     end
-    S[1] = I[1] + l_end_insertion
-    I[1] = I[1] + l_extend_insertion
+    S[1] = I[1] + L_END_INSERTION
+    I[1] = I[1] + L_EXTEND_INSERTION
+    for i in 1:length(S)
+      if typeof(expected[i]) <: AbstractBarcodeState
+        push!(S[i], observation.value)
+      end
+    end
   end
   best_tag_trace = max(I[n+1], S[n], D[n])
   return (best_tag_trace.score, best_tag_trace.tag)
+end
 end
