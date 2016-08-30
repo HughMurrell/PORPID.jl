22fb9a3dcc10ccfbe415b8716149b3ed59dffe86
unknown <numzane99@gmail.com>
2016-08-30 15:58:25 +0200

    First implementation of output to files

--- HMMIDs.jl
+++ HMMIDs.jl
@@ -8,6 +8,9 @@ using Nucleotides
 using Observations
 using ProfileHMMModel
 
+const SCORE_THRESHOLD = log(0.01) * 2 - 1e^-10
+const OUTPUT_FOLDER = "output"
+
 function py_index_to_julia(py_index, length, bound=false)
   if py_index < 0
     py_index += length
@@ -31,6 +34,7 @@ function process(json_file)
   # Options
   model = SmithWaterman
   do_reverse_complement = true
+  output_to_file = true
   if haskey(params, "options")
     options = params["options"]
     if haskey(options, "algorithm")
@@ -41,12 +45,22 @@ function process(json_file)
     if haskey(options, "do_reverse_complement")
       do_reverse_complement = options["do_reverse_complement"]
     end
+    if haskey(options, "output_to_file")
+      output_to_file = options["output_to_file"]
+    end
   end
 
   # Convert Patterns to StateSequences
   for plex in params["multiplexes"]
     reference_state_array = States.string_to_state_array(plex["reference"])
     plex["reference_state_array"] = reference_state_array
+    tag_length = 0
+    for state in reference_state_array
+      if typeof(state) <: AbstractBarcodeState
+        tag_length = tag_length + 1
+      end
+    end
+    plex["tag_length"] = tag_length
   end
 
   for file_name in params["files"]
@@ -60,11 +74,11 @@ function process(json_file)
 
       # Find best matching plex (group in a multiplexed sample)
       best_plex_score = -Inf
-      best_plex = "None"
+      best_plex = Union{}
+      best_plex_name = "None"
       best_tag = "None"
       for plex in params["multiplexes"]
         score, tag = model.extract_tag(observations, plex["reference_state_array"])
-
         if do_reverse_complement
           rc_observations = Observations.reverse_complement(observations)
           rc_score, rc_tag = model.extract_tag(rc_observations, plex["reference_state_array"])
@@ -77,12 +91,41 @@ function process(json_file)
         printif(params, "print_all_scores", "$(plex["name"]) $(round(score, 2)) $(join(map(string, tag), ""))\n")
         if score > best_plex_score
           best_plex_score = score
-          best_plex = plex["name"]
+          best_plex = plex
+          best_plex_name = plex["name"]
           best_tag = tag
         end
       end
-      printif(params, "print_plex", "\t$(best_plex)")
-      printif(params, "print_tag", "\t$(join(map(string, best_tag), ""))")
+
+      str_tag = join(map(string, best_tag), "")
+      if output_to_file
+        str_seq = join(map(string, sequence.seq), "")
+        str_quality = join(map(quality_to_char, sequence.quality), "")
+        folder_name = splitext(basename(file_name))[1]
+        
+        if !isdir("$OUTPUT_FOLDER")
+          mkdir("$OUTPUT_FOLDER")
+        end
+        if !isdir("$OUTPUT_FOLDER/$folder_name")
+          mkdir("$OUTPUT_FOLDER/$folder_name")
+        end
+        if !isdir("$OUTPUT_FOLDER/$folder_name/$best_plex_name")
+          mkdir("$OUTPUT_FOLDER/$folder_name/$best_plex_name")
+        end
+
+        if best_plex_score > best_plex["tag_length"] * log(0.25) + SCORE_THRESHOLD
+          open("$OUTPUT_FOLDER/$folder_name/$best_plex_name/$str_tag.fastq", "a") do f
+            write(f, "@$(sequence.label)($(round(best_plex_score, 2)))\n$str_seq\n+$(sequence.label)\n$str_quality\n")
+          end
+        else
+          open("$OUTPUT_FOLDER/$folder_name/$best_plex_name/REJECTS.fastq", "a") do f
+            write(f, "@$(sequence.label)($(round(best_plex_score, 2)))\n$str_seq\n+$(sequence.label)\n$str_quality\n")
+          end
+        end
+      end
+
+      printif(params, "print_plex", "\t$(best_plex_name)")
+      printif(params, "print_tag", "\t$str_tag")
       printif(params, "print_score", "\t$(round(best_plex_score, 2))")
       println()
     end
