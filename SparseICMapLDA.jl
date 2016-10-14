671f3a28b1a2b9e64da7bfe91d3ea67b9c1aca0c
unknown <numzane99@gmail.com>
2016-10-14 15:54:41 +0200

    First implementation of core LDA code

--- SparseICMapLDA.jl
+++ SparseICMapLDA.jl
@@ -1,4 +1,35 @@
 module SparseICMapLDA
+  export LDA
 
-  
+  const CONCENTRATION = 0.5
+
+  function LDA(cl_matrix, counts=zeros(Int32, 0, 1))
+    rows = size(cl_matrix, 1)
+    cols = size(cl_matrix, 2)
+    #theta must be a 1-by-x matrix to make element-wise multiplication work correctly
+    theta = ones(1, cols) / cols
+    phi = spzeros(rows, cols)
+    #counts must be a <rows>-by-1 matrix for the same reason
+    if size(counts, 1) == 0
+      counts = ones(Int32, rows, 1)
+    end
+
+    iterations = 100
+    for i in 1:iterations
+      for r in 1:rows
+        phi[r,:] = norm(theta .* cl_matrix[r,:])
+      end
+      for c in 1:cols
+        theta[1,c] = sum(phi[:,c] .* counts) + CONCENTRATION
+      end
+      theta = norm(theta)
+    end
+
+    return phi
+  end
+
+  function norm(v)
+    return v ./ sum(v)
+  end
+
 end
